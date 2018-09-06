d812df7cc9b0944c1a64051b937fe56d207d25e4
zhengbigbig <zhengbigbig@github.com>
2018-09-06 10:15:22 +0800

    Welcome.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome'
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root2')
+    <Welcome/>,
+    document.getElementById('root')
 );
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+
